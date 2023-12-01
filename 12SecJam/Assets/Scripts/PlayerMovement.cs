b2866d11f818271e1f8632cfdc6765f50ffdbb19
lksalto <saltolks@gmail.com>
2023-12-01 15:04:47 -0300

    teste

---
+++ 12SecJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed = 15f;
+    public Vector2 forceToApply;
+    public float forceDamping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+        Vector2 moveForce = PlayerInput;
+        moveForce += forceToApply;
+        forceToApply /= forceDamping;
+        if(Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+        {
+            forceToApply = Vector2.zero;
+        }
+        rb.velocity = moveForce;
+    }
+}
